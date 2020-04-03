5dff8eb52ecdcc5f66ab6bfb067ace24b851a972
Henric <henric.bjorkvall@gmail.com>
2020-04-03 15:10:05 +0200

    yes

--- public/themes/Gatenhielmska/front-page.php
+++ public/themes/Gatenhielmska/front-page.php
@@ -40,41 +40,37 @@ else : ?>
     <section class="events">
         <h2>Evenemang & Utställningar</h2>
 
-        <div class="event-cards">
-            <?php foreach ($events as $post) : setup_postdata($post); ?>
-
-                <?php $eventTypes = get_the_terms($post, 'event-type'); ?>
-
-                <div class="event-card">
-                    <div class="event-date">
-                        <?php if (get_field('date')) : ?>
-                            <p><?php the_field('date'); ?></p>
-                        <?php endif; ?>
-                        <?php if (get_field('time')) : ?>
-                            <p><?php the_field('time'); ?></p>
-                        <?php endif; ?>
-                    </div>
-                    <div class="event-info">
-                        <p>
-                            <?php foreach ($eventTypes as $eventType) : ?>
-                                <a class="event-type" href="<?php echo get_term_link($eventType) ?>"><?php echo $eventType->name ?></a>
-                            <?php endforeach; ?>
-                        </p>
-                        <h1 class="event-name"><?php the_title(); ?></h1>
-                        <?php if (has_post_thumbnail()) {
-                            the_post_thumbnail('medium');
-                        } ?>
-                        <button class="event-read-more-button"><span class="event-read-more-span">Läs mer..</span></button>
-                        <button class="ticket-button">Köp Biljett</button>
-                    </div>
+        <div class="event-card-wrapper">
+            <div class="event-card">
+                <div class="event-date">
+                    <p>12</p>
+                    <p>maj</p>
                 </div>
-            <?php endforeach; ?>
-        </div>
+                <div class="event-thumbnail">
+                    <img class="thumbnail-image" src="<?php echo get_bloginfo('template_directory'); ?>/assets/images/bannerimage.png" />
+                </div>
+                <div class="event-short-info">
+                    <p>Type of event</p>
+                    <h1>Name of event</h1>
+                </div>
+            </div>
+            <div class="event-card-expanded">
+                <p>Type of event</p>
+                <h1>Name of event</h1>
+                <p>Information about the event</p>
+                <p>Välkomna!</p>
+                <p>Entre: 100kr</p>
+                <p>Student/Pensionär: 35kr</p>
+                <p>Livestream: </p>
+                <p>Länk till livestream</p>
+            </div>
 
-    <?php endif; ?>
-    <button class="more-news-button">Alla evenemang</button>
+            <div class="event-buttons">
+                <button class="event-read-more">Läs mer</button>
+                <button class="buy-ticket">Köp biljett</button>
+            </div>
+        </div> <?php endif; ?>
     </section>
-
     <section class="news-letter">
         <div class="news-letter-box">
             <h1>Nyhetsbrev</h1>
