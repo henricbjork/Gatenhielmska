90d9bb16a7beea02ed9f24f1e533a27349dd0d93
Viktor <viktorpuke@gmail.com>
2020-04-07 20:27:31 +0200

    Working on about. issues with image save in dashboard.

---
+++ public/themes/Gatenhielmska/fields/timeline.php
@@ -0,0 +1,88 @@
+<?php
+
+if (function_exists('acf_add_local_field_group')) :
+
+    acf_add_local_field_group(array(
+        'key' => 'group_5e8c9678bec9a',
+        'title' => 'Timeline',
+        'fields' => array(
+            array(
+                'key' => 'field_5e8c96830d2e6',
+                'label' => 'slides',
+                'name' => 'slides',
+                'type' => 'repeater',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'collapsed' => '',
+                'min' => 0,
+                'max' => 0,
+                'layout' => 'table',
+                'button_label' => '',
+                'sub_fields' => array(
+                    array(
+                        'key' => 'field_5e8c982d0d2e7',
+                        'label' => 'Year',
+                        'name' => 'year',
+                        'type' => 'text',
+                        'instructions' => 'Write year',
+                        'required' => 1,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'default_value' => '',
+                        'placeholder' => '',
+                        'prepend' => '',
+                        'append' => '',
+                        'maxlength' => '',
+                    ),
+                    array(
+                        'key' => 'field_5e8c98530d2e8',
+                        'label' => 'Content',
+                        'name' => 'content',
+                        'type' => 'textarea',
+                        'instructions' => 'Write what happened that year',
+                        'required' => 1,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'default_value' => '',
+                        'placeholder' => '',
+                        'maxlength' => '',
+                        'rows' => '',
+                        'new_lines' => 'br',
+                    ),
+                ),
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'page_template',
+                    'operator' => '==',
+                    'value' => 'page-templates/page-about.php',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ));
+
+endif;
