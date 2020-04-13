8f629c9b6cc74225b2030a8403a3d92528ec77b6
Henric <henric.bjorkvall@gmail.com>
2020-04-13 14:41:50 +0200

    added basic 404 page

---
+++ public/themes/Gatenhielmska/404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<section class="no-page">
+    <h1>Hoppsan!</h1>
+    <p>Den här sidan verkar inte finnas.</p>
+    <a href="/">Tillbaka till första sidan</a>
+</section>
+
+<?php get_footer(); ?>
