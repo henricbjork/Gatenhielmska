2c13e3d0ad8b1a5f197fa60810aa4e94b0854872
Henric <henric.bjorkvall@gmail.com>
2020-04-15 18:31:57 +0200

    mixins

--- public/themes/Gatenhielmska/page-templates/page-gallery.php
+++ public/themes/Gatenhielmska/page-templates/page-gallery.php
@@ -10,8 +10,8 @@
     <div class="gallery-images">
         <?php echo do_shortcode('[instagram-feed class=instagram-feed num=2 cols=1 customavatar="https://instagram.fgse1-1.fna.fbcdn.net/v/t51.2885-19/s320x320/91925031_224891455588047_2279748338406391808_n.jpg?_nc_ht=instagram.fgse1-1.fna.fbcdn.net&_nc_ohc=h3-ubl9VfXsAX-MfLwJ&oh=523e33e9681342ce094d71ccf938a109&oe=5EB940F7" showheader=true showfollow=false showbutton=false disablemobile=true imagepadding=4 imagepaddingunit=px]'); ?>
         <a href="https://www.instagram.com/Gathenhielmska2/"><button class="instagram-btn">GÅ TILL INSTAGRAM</button></a>
-        <button class="gallery-more-btn">MINIMERA<img class="shrink-btn" src="<?php echo get_bloginfo('template_directory'); ?>/assets/images/minimize-white.svg" alt=""></button>
     </div>
+    <button class="gallery-less-btn">MINIMERA<img class="shrink-icon" src="<?php echo get_bloginfo('template_directory'); ?>/assets/images/minimize-white.svg" alt=""></button>
     <div class="video-header">
         <p>VIDEOS / LIVESTREAM</p>
         <img src="<?php echo get_bloginfo('template_directory'); ?>/assets/images/youtube-black.svg" alt="">
@@ -26,7 +26,7 @@
             <iframe width="100%" height="215" src="https://www.youtube.com/embed/izTaPcZZAXs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <a href="#"><button class="youtube-btn">GÅ TILL YOUTUBE</button></a>
         </div>
-        <button class="video-more-btn">MINIMERA<img class="video-shrink-btn" src="<?php echo get_bloginfo('template_directory'); ?>/assets/images/minimize-white.svg" alt=""><button>
     </div>
+    <button class="video-less-btn">MINIMERA<img class="video-shrink-icon" src="<?php echo get_bloginfo('template_directory'); ?>/assets/images/minimize-white.svg" alt=""></button>
 </section>
 <?php get_footer();
